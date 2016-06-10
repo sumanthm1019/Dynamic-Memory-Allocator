367cbb87f0fde45200c9bd05da0a9a6a2648fa0b
Sumanth Murali <sumanthm@ufl.edu>
2016-06-09 20:15:54 -0400

    Add files via upload
    Final Commit

--- allocator.c
+++ allocator.c
@@ -38,12 +38,17 @@ void copyBlock(block_t *src, block_t dst);
 int main() {
 
 	char *x = (char *) sCalloc(4, sizeof(char));
-	char *y = (char *) sMalloc(4 * sizeof(char));
-	free(x);
-	free(y);
+	int i;
+	for (i = 0; i < 32; i++) {
+		printf("x[%d] = %c\n", i, x[i]);
+	}
 	return 0;
 }
 
+/*When a block is found but is of larger size than requested,
+ *the large block is split into two blocks, one that is requested
+ *and another with the remaining space only if the remaining space is
+ sufficient to hold a block of data*/
 void blockSplit(block_t *b, size_t size) {
 
 	block_t *newnode;
@@ -60,6 +65,9 @@ void blockSplit(block_t *b, size_t size) {
 
 }
 
+/*Calloc calls the malloc function and initializes every byte of
+ * allocated memory to zero
+ */
 void *sCalloc(size_t num, size_t size) {
 	unsigned int *b;
 	int i;
@@ -73,6 +81,10 @@ void *sCalloc(size_t num, size_t size) {
 	return b;
 
 }
+/*The first fit algorithm, finds the first possible block of memory
+ * for the requested size. This way the performance is very high, however the block
+ * might not fit perfectly in the memory assigned leading to fragmentation
+ */
 block_t *findFirstFit(block_t **last, size_t size) {
 
 	block_t *b = *last;
@@ -85,16 +97,20 @@ block_t *findFirstFit(block_t **last, size_t size) {
 	return b;
 
 }
+/*The best fit algorithm tries to allocate a block of memory closest to the
+ * requested size thereby reducing fragmentation. The search for the block
+ * results in O(n) complexity reducing the performance.
+ */
 block_t *findBestFit(block_t **last, size_t size) {
 
 	block_t *b = *last;
 	size_t min = 100000000;
 	block_t *minb = NULL;
-	while(b){
-		if((min > abs(b->size - size)) && b->free == 1 && b->size >= size){
+	while (b) {
+		if ((min > abs(b->size - size)) && b->free == 1 && b->size >= size) {
 			min = abs(b->size - size);
 			minb = b;
-			if(min == 0)
+			if (min == 0)
 				break;
 		}
 		*last = b;
@@ -103,14 +119,18 @@ block_t *findBestFit(block_t **last, size_t size) {
 	return minb;
 
 }
+/*The worst fit algorithm tries to allocate the block in the largest memory
+ * available therefore reducing very small chunks of memory in the heap.
+ * Search takes O(n) time.
+ */
 block_t *findWorstFit(block_t **last, size_t size) {
 
 	block_t *b = *last;
 	size_t max = 0;
 	block_t *maxb = NULL;
-	while(b){
+	while (b) {
 
-		if((max < b->size) && b->free == 1 && b->size >= size){
+		if ((max < b->size) && b->free == 1 && b->size >= size) {
 			max = b->size;
 			maxb = b;
 		}
@@ -120,7 +140,9 @@ block_t *findWorstFit(block_t **last, size_t size) {
 	return maxb;
 
 }
-
+/*The extend heap function, extends the heap memory by extending the breakpoint
+ * and returning the older breakpoint address thereby creating new memory for usage.
+ */
 block_t *extendHeap(block_t *last, size_t size) {
 
 	block_t *b;
@@ -140,6 +162,11 @@ block_t *extendHeap(block_t *last, size_t size) {
 	return b;
 
 }
+/*Malloc using the first fit algorithm
+ * if a block is found using first fit, it is returned
+ * after using block split to reduce fragmentation
+ * else a new block is created using extend heap
+ */
 void *sMalloc(size_t size) {
 
 	block_t *b, *last;
@@ -179,7 +206,10 @@ block_t *getBlock(void *addr) {
 
 	return addr - BLOCK_SIZE;
 }
-
+/*Check if the address provided to the "Free" function
+ * is actually a valid address, one that was returned by a previous malloc
+ * This is done by storing the address in a pointer
+ */
 int isAddrValid(void *addr) {
 
 	if (start) {
@@ -190,6 +220,10 @@ int isAddrValid(void *addr) {
 	}
 	return 0;
 }
+/*Once a block is freed, an effort is made to coalesce memory blocks
+ * together to keep memory away from fragmentation. The function checks
+ * the next block and if it is free, fuses it with this block.
+ */
 
 block_t *blockFusion(block_t *b) {
 
@@ -202,6 +236,10 @@ block_t *blockFusion(block_t *b) {
 	return b;
 
 }
+/*Free function frees the memory block specified by the start
+ * address of the block. Block fusion with the previous and next
+ * memory blocks is performed once it is freed
+ */
 void free(void *addr) {
 
 	block_t *b;
@@ -224,6 +262,9 @@ void free(void *addr) {
 		}
 	}
 }
+/*Copy block of data from source to destination
+ * Used when realloc is called and a larger size is requested
+ */
 void copyBlock(block_t *src, block_t dst) {
 	char *s = src->ptr;
 	char *d = dst->ptr;
@@ -234,6 +275,9 @@ void copyBlock(block_t *src, block_t dst) {
 		d[i] = s[i];
 	}
 }
+/*Realloc checks if the requested size is less than the current size
+ * If it is larger, then the block is copied to a different region of memory
+ * and current block is freed */
 void *realloc(void *p, size_t size) {
 
 	size_t s;
